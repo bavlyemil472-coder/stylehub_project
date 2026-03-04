0972c08b79777358f7c74dfe5371bf5013ac0c51
Bavly emil <bavlyemil472@gmail.com>
2026-03-04 10:25:06 +0200

    Clean up urls.py by removing comment
    Removed commented line in urls.py

--- stylehub/accounts/urls.py
+++ stylehub/accounts/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import RegisterView, ProfileView # أضفنا ProfileView
+from .views import RegisterView, ProfileView 
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     
-    # المسار الجديد لعرض وتعديل بيانات المستخدم
     path('profile/', ProfileView.as_view(), name='profile'),
 ]
