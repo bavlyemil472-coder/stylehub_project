9351c9696a884321f2f53758e236685f8f490c86
Bavly emil <bavlyemil472@gmail.com>
2026-05-05 12:37:55 +0300

    Update permissions and query handling in views

--- stylehub/products/views.py
+++ stylehub/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from .models import product, category, Review
 from .serializers import ProductSerializer, CategorySerializer, ReviewSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
@@ -7,44 +7,47 @@ from .permissions import IsAdminOrReadOnly
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+
 class CategoryListCreateView(generics.ListCreateAPIView):
     queryset = category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [IsAdminOrReadOnly]
 
-# ✅ بعد — حطّ الكلام ده بدله
+
 class ProductListCreateView(generics.ListCreateAPIView):
     serializer_class = ProductSerializer
     permission_classes = [IsAdminOrReadOnly]
 
     def get_queryset(self):
         queryset = product.objects.filter(is_available=True).prefetch_related('p_images', 'variants')
-        
+
         category_id = self.request.query_params.get('category')
         if category_id:
             queryset = queryset.filter(category__id=category_id)
-        
+
         search = self.request.query_params.get('search')
         if search:
             queryset = queryset.filter(name__icontains=search)
-        
+
         return queryset
+
 
 class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = product.objects.all().prefetch_related('p_images', 'variants')
     serializer_class = ProductSerializer
     permission_classes = [AllowAny]
 
+
 class AddReviewView(APIView):
-    permission_classes = [IsAuthenticated] 
+    permission_classes = [AllowAny]  # ✅ guest مسموح
 
     def post(self, request, product_id):
         try:
             target_product = product.objects.get(id=product_id)
             data = request.data
-            
+
             review = Review.objects.create(
-                user=request.user,
+                user=request.user if request.user.is_authenticated else None,  # ✅
                 product=target_product,
                 rating=data.get('rating', 5),
                 comment=data.get('comment', '')
