26c6aec61135afc8c30459469ebafdcaf7a81156
Bavly emil <bavlyemil472@gmail.com>
2026-05-10 14:41:30 +0300

    Improve image handling in OrderItemSerializer
    Refactor image retrieval logic in OrderItemSerializer.

--- stylehub/orders/serializers.py
+++ stylehub/orders/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
-from .models import Order, OrderItem , ShippingRate
+from .models import Order, OrderItem, ShippingRate
+
 
 class OrderItemSerializer(serializers.ModelSerializer):
     total_price = serializers.SerializerMethodField()
-    
     product_name = serializers.ReadOnlyField(source='variant.product.name', default='Unknown Product')
     image = serializers.SerializerMethodField()
 
@@ -17,17 +17,23 @@ class OrderItemSerializer(serializers.ModelSerializer):
     def get_image(self, obj):
         try:
             product = obj.variant.product
-            if product.images.exists():
-                return product.images.first().image.url
+            if product.image:
+                return product.image.url
+            if product.p_images.exists():
+                return product.p_images.first().image.url
         except:
             return None
         return None
 
+
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True, read_only=True)
+
     class Meta:
         model = Order
         fields = '__all__'
+
+
 class ShippingRateSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShippingRate
