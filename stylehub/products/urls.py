84383968ce2a5ca3d5226b0d61d1a19fca6515d8
Bavly emil <bavlyemil472@gmail.com>
2026-05-14 10:50:27 +0300

    Update urls.py

--- stylehub/products/urls.py
+++ stylehub/products/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import CategoryListCreateView, ProductListCreateView, ProductDetailView, AddReviewView
+from .views import (
+    CategoryListCreateView, ProductListCreateView,
+    ProductDetailView, AddReviewView,
+    SectionListView, SubCategoryListView
+)
 
 urlpatterns = [
+    path('sections/', SectionListView.as_view(), name='section-list'),
     path('categories/', CategoryListCreateView.as_view(), name='category-list'),
+    path('subcategories/', SubCategoryListView.as_view(), name='subcategory-list'),
     path('products/', ProductListCreateView.as_view()),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
     path('products/<int:product_id>/add-review/', AddReviewView.as_view(), name='add-review'),
